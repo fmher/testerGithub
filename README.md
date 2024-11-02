6d157b70209ad3bdb1201cdb5f37d9ff7de89ab8
fmher <feumatthewher2@gmail.com>
2024-11-01 18:28:57 -0700

    random commit for github

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tester
 nothing of importance
+
+
+11-1-2024
+did some hw coding, basic html and css onlu
---
+++ main.js
@@ -0,0 +1,5 @@
+function addTwo (num, num2) {
+    return num + num2;
+}
+
+//  created a simple adding two number function 
